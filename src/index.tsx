2491ad735b22347e606edf2662432adbb5e9d2b8
Manojkumarpuranamgit <puranammanojkumar99@gmail.com>
2024-09-24 20:13:49 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css'; // Ensure you create this file or adjust as necessary
-import App from './App'; // Import your App component
+import './styles/index.css'; 
+import App from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
